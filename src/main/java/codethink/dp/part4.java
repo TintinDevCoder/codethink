7562d748952446a6a1d3c4c1b8ea5caeaf50ac33
SiYuan Ding <125371757+TintinDevCoder@users.noreply.github.com>
2025-07-02 10:42:43 +0800

    更改2

--- src/main/java/codethink/dp/part4.java
+++ src/main/java/codethink/dp/part4.java
@@ -1,5 +1,7 @@
 package codethink.dp;
 
+import java.util.Scanner;
+
 public class part4 {
     public int change(int amount, int[] coins) {
         int[] dp = new int[amount + 1];
@@ -23,5 +25,44 @@ public class part4 {
         }
         return dp[target];
     }
-
+    public int climbStar(int n, int m) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (i - j >= 0) dp[i] += dp[i - j];
+                else break;
+            }
+        }
+        return dp[n];
+    }
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = Integer.MAX_VALUE - 1;
+        }
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+        return dp[amount] == Integer.MAX_VALUE - 1 ? -1 : dp[amount];
+    }
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = Integer.MAX_VALUE - 1;
+        }
+        for (int i = 1; i * i <= n; i++) {
+            for (int j = i * i; j <= n; j++) {
+                dp[j] = Math.min(dp[j], dp[j - i * i] + 1);
+            }
+        }
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        part4 p = new part4();
+        int climbStar = p.coinChange(new int[]{2}, 3);
+        System.out.println(climbStar);
+    }
 }
